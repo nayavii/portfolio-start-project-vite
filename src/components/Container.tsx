96e2610c46de9d86764651fcd3fef335074063bc
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2025-11-24 18:12:21 +0100

    changed tablet padding in Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 export const Container = styled.div`
   max-width: 1090px;
@@ -6,5 +7,10 @@ export const Container = styled.div`
   min-height: 100%;
   padding: 0 15px;
   margin: 0 auto;
-  /* outline: 1px solid #e3e3e325; */
+  outline: 1px solid #e3e3e325;
+
+  @media ${theme.media.tablet} {
+    padding: 0 24px;
+
+  }
 `;
